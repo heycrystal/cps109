b8f5bd6fd879d81d22906f4e0c90ed2bc2da89a1
heycrystal <150701787+heycrystal@users.noreply.github.com>
2024-02-16 10:02:37 -0500

    Add files via upload

---
+++ list.py
@@ -0,0 +1,10 @@
+grades = [1, 2, 4, 8, 16, 32]
+type(grades)
+
+min(grades)
+max(grades)
+sum(grades)
+
+print(f' {grades}')
+print(f' The minimum of the list above is {min(grades)} and the maximum is {max(grades)}.')
+print(f' The total sum of the entire list is {sum(grades)}.')
