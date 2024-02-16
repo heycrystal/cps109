d93e8bdf6967ffef4ee635401fcb2cb3f78d07d0
heycrystal <150701787+heycrystal@users.noreply.github.com>
2024-02-16 10:57:55 -0500

    Update list.py

--- list.py
+++ list.py
@@ -8,3 +8,12 @@ sum(grades)
 print(f' {grades}')
 print(f' The minimum of the list above is {min(grades)} and the maximum is {max(grades)}.')
 print(f' The total sum of the entire list is {sum(grades)}.')
+
+for grade in grades:
+    print(grade)
+
+grad = int(input('Enter the grade here: '))
+if grad > 70:
+    print('You have at least a C average')
+else:
+    print('You are below a C average')
